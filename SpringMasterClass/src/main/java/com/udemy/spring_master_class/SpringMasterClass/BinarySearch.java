b62184ea99c8ec7b3c0d29b890dec15cac973ac1
selvakumar24896 <selvakumar24896@gmail.com>
2023-05-26 22:21:44 +0530

    step3 and step4

--- SpringMasterClass/src/main/java/com/udemy/spring_master_class/SpringMasterClass/BinarySearch.java
+++ SpringMasterClass/src/main/java/com/udemy/spring_master_class/SpringMasterClass/BinarySearch.java
@@ -1,13 +1,18 @@
 package com.udemy.spring_master_class.SpringMasterClass;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class BinarySearch {
 
+    @Autowired
     SortAlgorithm sortAlgorithm;
     public BinarySearch(SortAlgorithm algorithm){
         this.sortAlgorithm = algorithm;
     }
     public int searchElement(int[] arr, int element){
         sortAlgorithm.sortArray(arr);
-        return 0;
+        return 3;
     }
 }
--- SpringMasterClass/src/main/java/com/udemy/spring_master_class/SpringMasterClass/SpringMasterClassApplication.java
+++ SpringMasterClass/src/main/java/com/udemy/spring_master_class/SpringMasterClass/SpringMasterClassApplication.java
@@ -2,18 +2,16 @@ package com.udemy.spring_master_class.SpringMasterClass;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringMasterClassApplication {
 
 	public static void main(String[] args) {
-		searchInArray(new int[]{1,2,3,4,5}, 3);
-		SpringApplication.run(SpringMasterClassApplication.class, args);
-	}
-
-	private static void searchInArray(int[] arr, int i) {
-		BinarySearch bSearch = new BinarySearch(new BubbleSort());
-		bSearch.searchElement(arr, i);
+		ApplicationContext applicationContext= SpringApplication.run(SpringMasterClassApplication.class, args);
+		BinarySearch binarySearch = applicationContext.getBean(BinarySearch.class);
+		int bSearch = binarySearch.searchElement(new int[]{1,2,3,4,5}, 3);
+		System.out.println(bSearch);
 	}
 
 }
