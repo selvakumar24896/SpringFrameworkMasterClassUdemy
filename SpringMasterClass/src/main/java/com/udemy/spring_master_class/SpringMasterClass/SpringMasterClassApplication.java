d85b0138d16a19b6e0219ec8a06e1c8643d5806c
selvakumar24896 <selvakumar24896@gmail.com>
2023-05-26 20:26:53 +0530

    initial commit

---
+++ SpringMasterClass/src/main/java/com/udemy/spring_master_class/SpringMasterClass/SpringMasterClassApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.spring_master_class.SpringMasterClass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMasterClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMasterClassApplication.class, args);
+	}
+
+}
---
+++ SpringMasterClass/src/test/java/com/udemy/spring_master_class/SpringMasterClass/SpringMasterClassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.spring_master_class.SpringMasterClass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMasterClassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
