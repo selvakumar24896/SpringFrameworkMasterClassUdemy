e4e43c899a4b472102db090a5cd7d20dbd9d0146
selvakumar24896 <selvakumar24896@gmail.com>
2023-05-30 09:27:18 +0530

    @Primary annotation

--- SpringMasterClass/src/main/java/com/udemy/spring_master_class/SpringMasterClass/QuickSort.java
+++ SpringMasterClass/src/main/java/com/udemy/spring_master_class/SpringMasterClass/QuickSort.java
@@ -1,5 +1,10 @@
 package com.udemy.spring_master_class.SpringMasterClass;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
 public class QuickSort implements SortAlgorithm{
 
     @Override
