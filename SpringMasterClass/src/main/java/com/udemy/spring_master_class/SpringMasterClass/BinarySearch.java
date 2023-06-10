bc6daf8a8b81975b590bbedc96556d16757e120c
selvakumar24896 <selvakumar24896@gmail.com>
2023-06-10 18:39:49 +0530

    Step 13- auowire by @Qualifier

--- SpringMasterClass/src/main/java/com/udemy/spring_master_class/SpringMasterClass/BinarySearch.java
+++ SpringMasterClass/src/main/java/com/udemy/spring_master_class/SpringMasterClass/BinarySearch.java
@@ -1,20 +1,23 @@
 package com.udemy.spring_master_class.SpringMasterClass;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BinarySearch {
 
     @Autowired
-    SortAlgorithm bubbleSortAlgorithm;
-    public BinarySearch(SortAlgorithm algorithm){
-        super();
-        this.bubbleSortAlgorithm = algorithm;
-        System.out.println("!!!!Sort algorithm used is "+algorithm.getClass());
-    }
+    @Qualifier("bubble")
+    SortAlgorithm sortAlgorithm;
+//    public BinarySearch(SortAlgorithm algorithm){
+//        super();
+//        this.sortAlgorithm = algorithm;
+//        System.out.println("!!!!Sort algorithm used is "+algorithm.getClass());
+//    }
     public int searchElement(int[] arr, int element){
-        bubbleSortAlgorithm.sortArray(arr);
+        System.out.println("!!!!Sort algorithm used is "+sortAlgorithm.getClass());
+        sortAlgorithm.sortArray(arr);
         return 3;
     }
 }
--- SpringMasterClass/src/main/java/com/udemy/spring_master_class/SpringMasterClass/BubbleSortAlgorithm.java
+++ SpringMasterClass/src/main/java/com/udemy/spring_master_class/SpringMasterClass/BubbleSortAlgorithm.java
@@ -1,8 +1,10 @@
 package com.udemy.spring_master_class.SpringMasterClass;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+@Qualifier("bubble")
 public class BubbleSortAlgorithm implements SortAlgorithm{
 
     @Override
