23c24fe23c972c5bb2079966835d35413a7b12cf
selvakumar24896 <selvakumar24896@gmail.com>
2023-09-16 17:35:02 +0530

    Step 17- Bean lifecycle

--- SpringMasterClass/src/main/java/com/udemy/spring_master_class/SpringMasterClass/basics/BinarySearch.java
+++ SpringMasterClass/src/main/java/com/udemy/spring_master_class/SpringMasterClass/basics/BinarySearch.java
@@ -1,15 +1,21 @@
 package com.udemy.spring_master_class.SpringMasterClass.basics;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
 @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE) // New bean will be created whenever we instantiate the class
 public class BinarySearch {
 
+    private Logger LOGGER = LoggerFactory.getLogger(this.getClass());
     @Autowired
     @Qualifier("bubble")
     SortAlgorithm sortAlgorithm;
@@ -22,5 +28,16 @@ public class BinarySearch {
         System.out.println("!!!!Sort algorithm used is "+sortAlgorithm.getClass());
         sortAlgorithm.sortArray(arr);
         return 3;
+    }
+
+    @PostConstruct
+    public void  postConstruct(){
+        LOGGER.info("***In postConstruct");
+    }
+
+    @PreDestroy
+    public void  preDestroy(){
+        LOGGER.info("***In preDestroy");
+
     }
 }
