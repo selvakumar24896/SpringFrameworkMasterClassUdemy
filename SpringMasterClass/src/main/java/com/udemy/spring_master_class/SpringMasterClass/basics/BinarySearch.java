14405c1801a469eaf53fa2d8fe739b21a1ada803
selvakumar24896 <selvakumar24896@gmail.com>
2023-10-29 14:50:37 +0530

    section 8 - Spring boot web application

--- SpringMasterClass/src/main/java/com/udemy/spring_master_class/SpringMasterClass/basics/BinarySearch.java
+++ SpringMasterClass/src/main/java/com/udemy/spring_master_class/SpringMasterClass/basics/BinarySearch.java
@@ -8,8 +8,8 @@ import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 
 @Component
 @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE) // New bean will be created whenever we instantiate the class
