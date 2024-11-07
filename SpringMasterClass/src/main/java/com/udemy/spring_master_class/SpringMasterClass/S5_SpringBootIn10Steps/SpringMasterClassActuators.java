20e420bd790709cb87c0511a20c230b886d36d01
AnthonyRaj <Selvakumar.AnthonyRaj@agilysys.com>
2024-11-07 16:34:17 +0530

    spring Actuators

---
+++ SpringMasterClass/src/main/java/com/udemy/spring_master_class/SpringMasterClass/S5_SpringBootIn10Steps/SpringMasterClassActuators.java
@@ -0,0 +1,12 @@
+package com.udemy.spring_master_class.SpringMasterClass.S5_SpringBootIn10Steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringMasterClassActuators {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = SpringApplication.run(SpringMasterClassActuators.class);
+    }
+}
